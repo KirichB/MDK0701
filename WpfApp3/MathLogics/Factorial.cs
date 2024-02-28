d0d0c139cd4057560c47664fae0d66c977ea65c8
Kirill <kirichbugai@gmail.com>
2024-02-28 16:19:17 +0300

    Added factorial logic

--- WpfApp3/MathLogics/Factorial.cs
+++ WpfApp3/MathLogics/Factorial.cs
@@ -8,5 +8,15 @@ namespace WpfApp3.MathLogics
 {
     internal class Factorial
     {
+        public static int Calculate(int n)
+        {
+            int res = 1;
+            for (int i = 0; i < n; i++)
+            {
+
+                res *= i;
+            }
+            return res;
+        }
     }
 }
